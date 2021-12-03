1a567c076a72d35a4959a4d0d3f9cf60b4c50da0
Sergey <ser200026@mail.ru>
2021-12-03 21:58:21 +0700

    firstcommit

---
+++ FULL LEARN/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FULL_LEARN
+{
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+
+        {
+
+            double a = 123456.123456;
+
+            Console.WriteLine(@"{0:0.###########################################################}",a);
+
+        }
+    }
+}
